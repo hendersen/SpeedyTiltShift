f53a7559464eb037b9574c713983a594b9e80af1
hendersen <handianqi@gmail.com>
2017-10-06 14:43:12 -0700

    Update nativetiltshift-lib.cpp

--- app/src/main/cpp/nativetiltshift-lib.cpp
+++ app/src/main/cpp/nativetiltshift-lib.cpp
@@ -29,7 +29,7 @@ Java_meteor_asu_edu_speedytiltshift_SpeedyTiltShift_nativeTiltShiftNeon(JNIEnv *
                                                                     jint width, jint height,
                                                                     jint a0, jint a1, jint a2, jint a3, jfloat s_far, jfloat s_near
 ) {
-    int32x4_t sum_vec = vdupq_n_s32(0);
+
     jint *pixels = env->GetIntArrayElements(pixels_, NULL);
     long length = env->GetArrayLength(pixels_);
     jintArray pixelsOut = env->NewIntArray(length);
@@ -40,177 +40,114 @@ Java_meteor_asu_edu_speedytiltshift_SpeedyTiltShift_nativeTiltShiftNeon(JNIEnv *
 
     for(int i=0;i<length; i++){
         arrayIn[i]=pixels[i];
+        arrayOut[i]=pixels[i];
     }
+
 
     int arrlen=length/4;
-    float32_t AA[arrlen];
-    float32_t RR[arrlen];
-    float32_t GG[arrlen];
-    float32_t BB[arrlen];
+    jfloat sig=0;
 
-
-    float32_t * arrayInPtr = (float32_t *)arrayIn;
-    for(int i=0;i<arrlen;i++){
-        float32x4x4_t vecs=vld4q_f32(arrayInPtr);
-        float32x4_t A=vecs.val[0];
-        float32x4_t R=vecs.val[1];
-        float32x4_t G=vecs.val[2];
-        float32x4_t B=vecs.val[3];
-        AA[i]=A;
-        RR[i]=R;
-        GG[i]=G;
-        BB[i]=B;
-        arrayInPtr+=16;
-    }
-
-    int sig=0;
-    float32_t temp0;
-    float32_t temp1;
-    float32_t temp2;
-    float32_t temp3;
     for(int j=0;j<arrlen;j++){
-        if(j<(a0*width/4)){
+        float32x4_t sum_vecA = vdupq_n_f32(0);
+        float32x4_t sum_vecR = vdupq_n_f32(0);
+        float32x4_t sum_vecG = vdupq_n_f32(0);
+        float32x4_t sum_vecB = vdupq_n_f32(0);
+        if(j<=(a0*width/4)){
             sig=s_far;
-            int r=ceil(3*sig);
-            int size=(2*r+1)*(2*r+1);
-            int mid=(size-1)/2;
-            temp0=0;
-            temp1=0;
-            temp2=0;
-            temp3=0;
-            float32_t weight[size];
-            for(int m=0;m<2*r+1;m++){
-                for(int n=0;n<2*r+1;n++){
-                    weight[m*r+n]=exp(-(m*m+n*n)/(2*sig*sig))/(2*3.14*sig*sig);
-                }
-            }
-            for(int a=mid;a<size&&a+j-mid<arrlen;a++) {
-                temp0=vmla_f32(temp0,AA[j+a-mid],weight[a]);
-                temp1=vmla_f32(temp1,RR[j+a-mid],weight[a]);
-                temp2=vmla_f32(temp2,GG[j+a-mid],weight[a]);
-                temp3=vmla_f32(temp3,BB[j+a-mid],weight[a]);
-            }
-            for(int a=mid-1;a>0&&a>mid-j;a--){
-                temp0=vmla_f32(temp0,AA[j+a-mid],weight[a]);
-                temp1=vmla_f32(temp1,RR[j+a-mid],weight[a]);
-                temp2=vmla_f32(temp2,GG[j+a-mid],weight[a]);
-                temp3=vmla_f32(temp3,BB[j+a-mid],weight[a]);
-            }
-            arrayOut[4*j]=temp0;
-            arrayOut[4*j+1]=temp1;
-            arrayOut[4*j+2]=temp2;
-            arrayOut[4*j+3]=temp3;
-            break;
         }
-        if(j<a1*width/4){
+
+        if((a0*width/4)<j&&j<=(a1*width/4)){
             int len=floor(4*j/width);
             sig=s_far*((a1-len)/(a1-a0));
-            int r=ceil(sig*3);
-            int size=(2*r+1)*(2*r+1);
-            int mid=(size-1)/2;
-            temp0=0;
-            temp1=0;
-            temp2=0;
-            temp3=0;
-            float32_t weight[size];
-            for(int m=0;m<2*r+1;m++){
-                for(int n=0;n<2*r+1;n++){
-                    weight[m*r+n]=exp(-(m*m+n*n)/(2*sig*sig))/(2*3.14*sig*sig);
-                }
-            }
-            for(int a=mid;a<size&&a+j-mid<arrlen;a++) {
-                temp0=vmla_f32(temp0,AA[j+a-mid],weight[a]);
-                temp1=vmla_f32(temp1,RR[j+a-mid],weight[a]);
-                temp2=vmla_f32(temp2,GG[j+a-mid],weight[a]);
-                temp3=vmla_f32(temp3,BB[j+a-mid],weight[a]);
-            }
-            for(int a=mid-1;a>0&&a>mid-j;a--){
-                temp0=vmla_f32(temp0,AA[j+a-mid],weight[a]);
-                temp1=vmla_f32(temp1,RR[j+a-mid],weight[a]);
-                temp2=vmla_f32(temp2,GG[j+a-mid],weight[a]);
-                temp3=vmla_f32(temp3,BB[j+a-mid],weight[a]);
-            }
-            arrayOut[4*j]=temp0;
-            arrayOut[4*j+1]=temp1;
-            arrayOut[4*j+2]=temp2;
-            arrayOut[4*j+3]=temp3;
+        }
+
+        if((a1*width/4)<j&&j<=(a2*width/4)){
             break;
         }
-        if(j<a2*width/4){
-            arrayOut[4*j]=AA[j];
-            arrayOut[4*j+1]=RR[j];
-            arrayOut[4*j+2]=GG[j];
-            arrayOut[4*j+3]=BB[j];
-            break;
-        }
-        if(j<a3*width/4){
+
+        if((a2*width/4)<j&&j<=(a3*width/4)){
             int len=floor(4*j/width);
             sig=s_near*((len-a2)/(a3-a2));
-            int r=ceil(sig*3);
-            int size=(2*r+1)*(2*r+1);
-            int mid=(size-1)/2;
-            temp0=0;
-            temp1=0;
-            temp2=0;
-            temp3=0;
-            float32_t weight[size];
-            for(int m=0;m<2*r+1;m++){
-                for(int n=0;n<2*r+1;n++){
-                    weight[m*r+n]=exp(-(m*m+n*n)/(2*sig*sig))/(2*3.14*sig*sig);
-                }
-            }
-            for(int a=mid;a<size&&a+j-mid<arrlen;a++) {
-                temp0=vmla_f32(temp0,AA[j+a-mid],weight[a]);
-                temp1=vmla_f32(temp1,RR[j+a-mid],weight[a]);
-                temp2=vmla_f32(temp2,GG[j+a-mid],weight[a]);
-                temp3=vmla_f32(temp3,BB[j+a-mid],weight[a]);
-            }
-            for(int a=mid-1;a>0&&a>mid-j;a--){
-                temp0=vmla_f32(temp0,AA[j+a-mid],weight[a]);
-                temp1=vmla_f32(temp1,RR[j+a-mid],weight[a]);
-                temp2=vmla_f32(temp2,GG[j+a-mid],weight[a]);
-                temp3=vmla_f32(temp3,BB[j+a-mid],weight[a]);
-            }
-            arrayOut[4*j]=temp0;
-            arrayOut[4*j+1]=temp1;
-            arrayOut[4*j+2]=temp2;
-            arrayOut[4*j+3]=temp3;
-            break;
-
-        }
-        else{
-            sig=s_near;
-            int r=ceil(3*sig);
-            int size=(2*r+1)*(2*r+1);
-            int mid=(size-1)/2;
-            temp0=0;
-            temp1=0;
-            temp2=0;
-            temp3=0;
-            float32_t weight[size];
-            for(int m=0;m<2*r+1;m++){
-                for(int n=0;n<2*r+1;n++){
-                    weight[m*r+n]=exp(-(m*m+n*n)/(2*sig*sig))/(2*3.14*sig*sig);
-                }
-            }
-            for(int a=mid;a<size&&a+j-mid<arrlen;a++) {
-                temp0=vmla_f32(temp0,AA[j+a-mid],weight[a]);
-                temp1=vmla_f32(temp1,RR[j+a-mid],weight[a]);
-                temp2=vmla_f32(temp2,GG[j+a-mid],weight[a]);
-                temp3=vmla_f32(temp3,BB[j+a-mid],weight[a]);
-            }
-            for(int a=mid-1;a>0&&a>mid-j;a--){
-                temp0=vmla_f32(temp0,AA[j+a-mid],weight[a]);
-                temp1=vmla_f32(temp1,RR[j+a-mid],weight[a]);
-                temp2=vmla_f32(temp2,GG[j+a-mid],weight[a]);
-                temp3=vmla_f32(temp3,BB[j+a-mid],weight[a]);
-            }
-            arrayOut[4*j]=temp0;
-            arrayOut[4*j+1]=temp1;
-            arrayOut[4*j+2]=temp2;
-            arrayOut[4*j+3]=temp3;
         }
 
+        if((a3*width/4)<j){
+            sig=s_far;
+        }
+
+        int r=ceil(3*sig);
+        int size=(2*r+1)*(2*r+1);
+        int mid=(size-1)/2;
+        float32_t weight[size];
+        float32_t image[size*4];
+
+        for(jint m=0;m<2*r+1;m++){
+            for(int n=0;n<2*r+1;n++){
+                weight[m*r+n]=exp(-(m*m+n*n)/(2*sig*sig))/(2*3.14*sig*sig);
+            }
+        }//construct weight matrix
+
+        int loc_x=4*j%width;
+        int loc_y=floor(4*j/width);
+        float32_t * weightPtr=(float32_t *)weight;
+
+
+        for(int m=0;m<2*r+1;m++){
+            for(int n=0;n<2*r+1;n++){
+                if(loc_x-4*r+4*m<0||loc_x-4*r+4*m>width) {
+                    for(int a=0;a<4;a++){
+                        image[n*(2*r+1)+4*m+a]=0;
+                    }
+                }else{
+                    if(loc_y-r+n<0||loc_y-r+n>height){
+                        for(int a=0;a<4;a++){
+                            image[n*(2*r+1)+4*m+a]=0;
+                        }
+                    }
+                }
+            }
+
+        }//construct image matrix
+
+        float32_t * imagePtr=(float32_t *)image;
+
+        for(int i=0;i<(size/4);i++){
+            float32x4x4_t ivec=vld4q_f32(imagePtr);
+            float32x4_t wvec=vld1q_f32(weight);
+            sum_vecA = vmla_f32(sum_vecA,ivec.val[0],wvec);
+            sum_vecR = vmla_f32(sum_vecR,ivec.val[1],wvec);
+            sum_vecG = vmla_f32(sum_vecG,ivec.val[2],wvec);
+            sum_vecB = vmla_f32(sum_vecB,ivec.val[3],wvec);
+        }
+
+        float sumA=0;
+        float sumR=0;
+        float sumG=0;
+        float sumB=0;
+
+        sumA +=vgetq_lane_f32(sum_vecA,0);
+        sumA +=vgetq_lane_f32(sum_vecA,1);
+        sumA +=vgetq_lane_f32(sum_vecA,2);
+        sumA +=vgetq_lane_f32(sum_vecA,3);
+
+        sumR +=vgetq_lane_f32(sum_vecR,0);
+        sumR +=vgetq_lane_f32(sum_vecR,1);
+        sumR +=vgetq_lane_f32(sum_vecR,2);
+        sumR +=vgetq_lane_f32(sum_vecR,3);
+
+        sumG +=vgetq_lane_f32(sum_vecG,0);
+        sumG +=vgetq_lane_f32(sum_vecG,1);
+        sumG +=vgetq_lane_f32(sum_vecG,2);
+        sumG +=vgetq_lane_f32(sum_vecG,3);
+
+        sumB +=vgetq_lane_f32(sum_vecB,0);
+        sumB +=vgetq_lane_f32(sum_vecB,1);
+        sumB +=vgetq_lane_f32(sum_vecB,2);
+        sumB +=vgetq_lane_f32(sum_vecB,3);//calculate A,R,G,B distribute
+
+        arrayOut[j*4]=sumA;
+        arrayOut[j*4+1]=sumR;
+        arrayOut[j*4+2]=sumG;
+        arrayOut[j*4+3]=sumB;//cppy image value to arrayout
     }
     jint *h=new jint[length];
     for(int i=0;i<length;i++){
